14708b9b8143bed4431b7ed238b40331bd9d36b1
Luminous <shiyan077@hotmail.com>
2019-04-11 20:53:43 +0800

    添加Player 修改射线检测不到的bug 移植、优化碰撞检测

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Vector3 MousePosition;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            MousePosition = Input.mousePosition;
+            //从摄像机发出到点击坐标的射线
+            Ray ray = Camera.main.ScreenPointToRay(MousePosition);
+            RaycastHit hitinfo;
+
+            if (Physics.Raycast(ray, out hitinfo))
+            {
+                GameObject templetter = hitinfo.collider.gameObject;
+                Debug.Log("碰到的物体是" + templetter.name);
+                if (templetter.tag == "Letter")
+                {
+                    templetter.SendMessage("OnTouch");
+                    Debug.Log("OnTouch 点到");
+                }
+
+            }
+        }
+
+        
+    }
+}
