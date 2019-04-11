e66f55c3185eba1c7624b577819f98e76ff488cc
Luminous <shiyan077@hotmail.com>
2019-04-11 19:54:14 +0800

    碰撞检测

--- Assets/Scripts/Letter.cs
+++ Assets/Scripts/Letter.cs
@@ -5,6 +5,26 @@ using UnityEngine;
 public class Letter : MonoBehaviour
 {
 
+
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //从摄像机发出到点击坐标的射线
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitinfo;
+            if(Physics.Raycast(ray,out hitinfo))
+            {
+                GameObject templetter = hitinfo.collider.gameObject;
+                if(templetter.tag=="letter")
+                {
+                    OnTouch();
+                }
+            }
+        }
+    }
+
     // 字母 get set 
     private string letterContent;
     public string LetterContent
@@ -37,7 +57,14 @@ public class Letter : MonoBehaviour
     // 点击到该字母
     public void OnTouch() {
         ParentWord.CheckLetter(LetterContent);
+
         // ... to do: 改变颜色以提示
+
+    }
+
+    //重置状态
+    public void ResetStatus()
+    {
 
     }
 
