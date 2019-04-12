ace2be92b60730d21a020bbe4ff38e344b54d4f6
nuso <nelohjb@hotmail.com>
2019-04-12 16:58:24 +0800

    添加WordPanelBase基类
    WordPrefab: 单词物体Prefab

---
+++ Assets/Scripts/WordPanelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+
+public class WordPanelBase : MonoBehaviour {
+
+    public GameObject WordPrefab;
+
+
+    virtual public void ClearWord(WordBase wordBase) { }
+
+    virtual public void FocusOn(WordBase wordBase) { }
+}
