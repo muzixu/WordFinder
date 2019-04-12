3f0f5b6604849ae6e42e6d9b2dad8dc1ffce817c
nuso <nelohjb@hotmail.com>
2019-04-12 17:30:17 +0800

    修改测试脚本，测试WordBar与新的WordPanel.

--- Assets/Scripts/Tests/Test.cs
+++ Assets/Scripts/Tests/Test.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.Scripts;
 
 public class Test : MonoBehaviour {
 
     public WordPanel _WordPanelp;
+    public WordBar _WordBar;
 
 	void Start () {
-        _WordPanelp.AddWord("Hello");
-        _WordPanelp.AddWord("nuso");
+        WordBase word1 = new WordBase();
+        WordBase word2 = new WordBase();
+        word1.WordRaw = "WORD";
+        word2.WordRaw = "GOD";
+
+        _WordPanelp.AddWord(word1);
+        _WordPanelp.AddWord(word2);
+
+        _WordBar.UpdateWord(word2);
     }
 	
 
